6ac98d884ad0dbfd328854fb84765a3f44751ba1
Alex Guerra <alexander.a.guerra@gmail.com>
2024-12-26 05:36:19 -0600

    Testing env functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 
 export default function Home() {
+    console.log(process.env)
     return (
         <div>
             <h1>Home</h1>
+            {process.env.OPENAI_API_KEY}
         </div>
     );
 }
